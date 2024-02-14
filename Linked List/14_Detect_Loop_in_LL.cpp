acb8ea793062a3dc64addab0e5341b7a550db70d
Anish Mishra <mishraanish8757@gmail.com>
2024-02-14 22:50:28 +0530

    First Commit

---
+++ Linked List/14_Detect_Loop_in_LL.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node * next;
+
+    public:
+    Node(int data, Node * next){
+        this->data = data;
+        this->next = next;
+    }
+
+    public:
+    Node(int data)
+    {
+      this->data = data;
+      next = nullptr;
+    }
+};
+
+Node* Convert_Arr2LL(vector<int> & arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}  
+
+void Traverse_LL(Node* head){
+
+    Node* temp = head;
+    while(temp != nullptr)
+    {
+        cout<<temp->data<< " ";
+        temp = temp->next;
+    }  
+}
+
+int main()
+{
+    vector<int> arr = {9,19,1,29,8};
+    Node* head = Convert_Arr2LL(arr);
+
+             
+  return 0;
+}
