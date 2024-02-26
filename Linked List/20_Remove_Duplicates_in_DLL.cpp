5289cf5c098199819052619bc1d386ff8203c28e
Anish Mishra <mishraanish8757@gmail.com>
2024-02-26 22:27:11 +0530

    Update Code

--- Linked List/20_Remove_Duplicates_in_DLL.cpp
+++ Linked List/20_Remove_Duplicates_in_DLL.cpp
@@ -1,4 +1,5 @@
-/* You’re given a doubly-linked list and a key 'k'. Delete all the nodes having data equal to ‘k’.*/
+/* You are given a sorted doubly linked list of size 'n'. Remove all the duplicate nodes present 
+in the linked list.*/
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -50,28 +51,43 @@ void Traverse_LL(Node* head){
 
 //New Concepts start from here.
 /*
-Brute Frute Solution : Iterate over the Doubly Linked List once and delete the node while traversal.
+Brute Frute Solution : Iterate over the Doubly Linked List and Change the links using a next non-duplicate node.
 Step 1 : Iterate through the linked list and check if temp's data is equal to key.
 Step 2 : Once the data is matching then check if temp is at head or not in case of head we need to move it by 1.
 Step 3 : Store the prevNode and the nextNode and check if they are not null then perform the link changes.
 Step 4 : Delete the temp and move temp to nextNode after the link changes are done.
 Step 5 : If data is not matching then move temp to the next node.
 
-Time Complexity : O(N)  
-Space Complexity : O(1) 
+Time Complexity : O(N)
+Space Complexity : O(1)
+
+Time Complexity is O(N) because outer loop is touching only the non-duplicate nodes and inner loop is touching
+only the duplicate nodes hence both of these travel only upto the length of the doubly linked list.
 */
-int findPairs(Node* head, int key) {
-    return 0;
-    
+Node * removeDuplicates(Node *head){
+    Node* temp = head, *nextNode = NULL;
+    while (temp != NULL){
+        nextNode = temp->next;
+        while(nextNode != NULL && nextNode->data == temp->data){
+            Node* duplicate = nextNode;
+            nextNode = nextNode->next;
+            delete duplicate;
+        }
+        temp->next = nextNode;
+        if(nextNode) nextNode->back = temp;
+
+        temp = temp->next;
+    }
+    return head;    
 }
 
 int main (){
 
-    vector<int> arr = {1,2,1,1,4,1,6,7,1};
+    vector<int> arr = {1,1,1,4,4,6,7,7};
     Node* head = Convert_Arr2DLL(arr);
 
-    // head = deleteAllOccurrences(head,1);
-    // Traverse_LL(head);
+    Node* answer = removeDuplicates(head);
+    Traverse_LL(answer);
 
     return 0;
 }
