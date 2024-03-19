2355e4746778496165a44cc30c01815d03a3531f
Anish Mishra <mishraanish8757@gmail.com>
2024-03-19 23:34:20 +0530

    First Commit

---
+++ Binary Search/3_Search_Insert_Position.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int searchInsert(vector<int>& arr, int x) {
+    int n = arr.size();
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] >= x) { // maybe an answer
+            ans = mid;
+            high = mid - 1; //look for smaller index on the left
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+
+int main()
+{
+    vector<int> arr = {1, 2, 4, 7};
+    int x = 6;
+    int ind = searchInsert(arr, x);
+    cout << "The index is: " << ind << "\n";
+    return 0;
+}
